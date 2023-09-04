3367e85cd99caf748eb418f41ba4ac956982f923
sinan <sinancivak00@gmail.com>
2023-09-05 00:05:55 +0300

    GenericRepository Eklendş

---
+++ SC.DataLayer/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using SC.DataLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC.DataLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _db;
+
+        public GenericRepository(Context db)
+        {
+            _db = db;
+        }
+
+        public void Delete(T t)
+        {
+            _db.Remove(t);
+            _db.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+        public List<T> GetList()
+        {
+         return _db.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _db.Add(t);
+            _db.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _db.Update(t);
+            _db.SaveChanges();
+        }
+    }
+}
