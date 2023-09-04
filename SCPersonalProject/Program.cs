5101ab046414cf4885efcd77d6e5eb77b1cba34d
sinan <sinancivak00@gmail.com>
2023-09-04 22:49:41 +0300

    .

--- SCPersonalProject/Program.cs
+++ SCPersonalProject/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SC.DataLayer;
+
 namespace SCPersonalProject
 {
     public class Program
@@ -8,6 +11,8 @@ namespace SCPersonalProject
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
