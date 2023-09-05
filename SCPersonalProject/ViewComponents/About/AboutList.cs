8eec1f32fb4fc47d0a523763e81c3c208727aebd
sinan <sinancivak00@gmail.com>
2023-09-05 23:33:59 +0300

    View Component

---
+++ SCPersonalProject/ViewComponents/About/AboutList.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SC.Bussines.Content;
+using SC.Bussines.Services;
+using SC.DataLayer.EntityFramework;
+
+namespace SCPersonalProject.ViewComponents.About
+{
+    public class AboutList : ViewComponent
+    { 
+       private readonly IAboutService _aboutService;
+
+        public AboutList(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values=_aboutService.TGetList();    
+            
+            return View(values);
+        }
+    }
+}
+
---
+++ SCPersonalProject/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SC.Bussines.Services;
+
+namespace SCPersonalProject.ViewComponents.Service
+{
+    public class ServiceList:ViewComponent
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceList(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values=_serviceService.TGetList();
+
+            return View(values);
+        }
+
+    }
+}
