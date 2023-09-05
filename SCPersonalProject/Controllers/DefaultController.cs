3d9d17020ce222eaa87ea1d63aa555ec975395f5
sinan <sinancivak00@gmail.com>
2023-09-05 23:33:44 +0300

    Ana sayfa controller

---
+++ SCPersonalProject/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCPersonalProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+
+            return PartialView();
+        }
+
+        public PartialViewResult HeroSectionPartial()
+        {
+
+            return PartialView();
+        }
+    }
+}
