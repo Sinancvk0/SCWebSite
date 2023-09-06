4eb7d58eb1bb18771da2b2cb422117be36ded529
sinan <sinancivak00@gmail.com>
2023-09-06 22:32:02 +0300

    contact  addscoped eklendi

---
+++ SC.Bussines/Container/ExtensionsDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SC.Bussines.Content;
+using SC.Bussines.Services;
+using SC.DataLayer.Abstract;
+using SC.DataLayer.EntityFramework;
+
+namespace SC.Bussines.Container
+{
+    public class ExtensionsDb
+    {
+     
+
+        public static void ContainerDependencies(IServiceCollection services)
+        {
+
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IAboutDal, EfAboutDal>();
+
+            services.AddScoped<IServiceService, ServiceManager>();
+            services.AddScoped<IServiceDal, EfServiceDal>();
+
+            services.AddScoped<IWorkService, WorkManager>();
+            services.AddScoped<IWorkDal, EfWorkDal>();
+
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IContactDal, EfContactDal>();
+
+
+        }
+    }
+}
