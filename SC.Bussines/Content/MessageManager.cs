173dda0368dcc5b8c68b4779c55176221d804a1f
sinan <sinancivak00@gmail.com>
2023-09-07 22:47:30 +0300

    Mesaj işlmeleri

---
+++ SC.Bussines/Content/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using SC.Bussines.Services;
+using SC.DataLayer.Abstract;
+using SC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC.Bussines.Content
+{
+    public class MessageManager :IGenericService<Message>,IMessageService
+    {
+        private readonly IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void TAdd(Message t)
+        {
+            _messageDal.Insert(t);
+        }
+
+        public void TDelete(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Message t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
