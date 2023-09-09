10302a36ebf6f61e1a1f65e3c6ef16ccb18c6c08
sinan <sinancivak00@gmail.com>
2023-09-09 17:48:04 +0300

    Admin contact eklendi

---
+++ SCPersonalProject/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SC.Bussines.Services;
+using SC.Models;
+
+namespace SCPersonalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contact;
+
+        public ContactController(IContactService contact)
+        {
+            _contact = contact;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            var values=_contact.TGetList();
+
+            return Json(values);
+
+        }
+
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult AddContact(Contact contact)
+        {
+            _contact.TAdd(contact);
+
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult DeleteContact(int id)
+        {
+            var values=_contact.TGetById(id);
+            _contact.TDelete(values);
+
+            return Ok();
+
+        }
+    }
+}
