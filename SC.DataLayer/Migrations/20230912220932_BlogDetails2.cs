27cde3b93ff80d5fa653abd24f89c0dfd2d08565
sinan <sinancivak00@gmail.com>
2023-09-13 01:12:52 +0300

    .

---
+++ SC.DataLayer/Migrations/20230912220932_BlogDetails2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SC.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogDetails2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content2",
+                table: "BlogDetaills",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl2",
+                table: "BlogDetaills",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl3",
+                table: "BlogDetaills",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content2",
+                table: "BlogDetaills");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl2",
+                table: "BlogDetaills");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl3",
+                table: "BlogDetaills");
+        }
+    }
+}
---
+++ SCPersonalProject/Controllers/BlogRetroController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SC.Bussines.Services;
+using SC.DataLayer;
+
+namespace SCPersonalProject.Controllers
+{
+    public class BlogRetroController : Controller
+    {
+        private readonly IBlogService _blogService;
+        private readonly AppDbContext _appDbContext;
+        private readonly IBlogDetailsService _blogDetailsService;
+
+        public BlogRetroController(IBlogService blogService, AppDbContext appDbContext, IBlogDetailsService blogDetailsService)
+        {
+            _blogService = blogService;
+            _appDbContext = appDbContext;
+            _blogDetailsService = blogDetailsService;
+        }
+        //Inculude ile Categoryden Details ulaştık 
+        public IActionResult Index()
+        {
+            var values = _blogService.TGetList();
+
+            var blogvalue = _appDbContext.GetBlogCategories.Include(x => x.BlogDetaills).ToList();
+
+            return View(blogvalue);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        
+        //Blog detay Idsine göre Blog sayfa detayına gittik
+        {
+            var blogDetail = _blogDetailsService.TGetById(id);
+
+            if (blogDetail == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(blogDetail);
+
+        }
+    }
+}
