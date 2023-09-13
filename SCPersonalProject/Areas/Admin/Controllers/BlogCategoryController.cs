30284ab098f7b7cd2e84823726ce9ccae0ce1231
sinan <sinancivak00@gmail.com>
2023-09-13 23:26:12 +0300

    Blog Detayları Yapıldı

---
+++ SCPersonalProject/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SC.Bussines.Services;
+using SC.DataLayer.Abstract;
+using SC.Models;
+
+namespace SCPersonalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogCategoryController : Controller
+    {
+        private readonly IBlogService _blogservice;
+
+        public BlogCategoryController(IBlogService blogservice)
+        {
+            _blogservice = blogservice;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            try
+            {
+
+                var BlogCategory = _blogservice.TGetList();
+                return Json(BlogCategory);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Veriler alınamadı: " + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory( BlogCategory blogCategory)
+        {
+            _blogservice.TAdd(blogCategory);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCategory(int id)
+        {
+
+           var values=_blogservice.TGetById(id);    
+
+            _blogservice.TDelete(values);   
+
+            return Ok();
+        }
+    }
+}
---
+++ SCPersonalProject/Areas/Admin/Controllers/BlogDetailsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SC.Bussines.Services;
+using SC.Models;
+
+namespace SCPersonalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogDetailsController : Controller
+    {
+
+        private readonly IBlogDetailsService _blogDetailsService;
+
+
+        public BlogDetailsController(IBlogDetailsService blogDetailsService)
+        {
+            _blogDetailsService = blogDetailsService;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            var values = _blogDetailsService.TGetList();
+
+            return Json(values);
+        }
+
+        [HttpPost]
+
+        public IActionResult DeleteDetails(int id)
+        {
+            var values = _blogDetailsService.TGetById(id);
+
+            _blogDetailsService.TDelete(values);
+
+            return Ok();
+
+        }
+
+        [HttpGet]
+        public IActionResult AddDetails()
+        {
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddDetails(BlogDetaills blogDetaills, List<IFormFile> images)
+        {
+            //Blog ImagerUrl 
+            for (int i = 0; i < images.Count; i++)
+            {
+                var image = images[i];
+                if (image != null && image.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        image.CopyTo(stream);
+                    }
+
+
+                    if (i == 0)
+                    {
+                        blogDetaills.ImageUrl = "/images/" + fileName;
+                    }
+                    else if (i == 1)
+                    {
+                        blogDetaills.ImageUrl2 = "/images/" + fileName;
+                    }
+                    else if (i == 2)
+                    {
+                        blogDetaills.ImageUrl3 = "/images/" + fileName;
+                    }
+         
+                }
+            }
+
+            _blogDetailsService.TAdd(blogDetaills);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
