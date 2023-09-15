f72af10b5fcf039e91aef2c9280cfdda5085ca7b
sinan <sinancivak00@gmail.com>
2023-09-15 17:43:04 +0300

    Blog Details Sayfası düzenlendi

---
+++ SCPersonalProject/ViewComponents/BlogDetails/BlogDetails.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SC.Bussines.Services;
+using SC.DataLayer;
+
+namespace SCPersonalProject.ViewComponents.BlogDetails
+{
+    public class BlogDetails:ViewComponent
+    {
+        private readonly IBlogService _blogService;
+        private readonly AppDbContext _appDbContext;
+        public BlogDetails(IBlogService blogService, AppDbContext appDbContext)
+        {
+            _blogService = blogService;
+            _appDbContext = appDbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.TGetList();
+
+            var blogvalue = _appDbContext.GetBlogCategories.Include(x => x.BlogDetaills).ToList();
+            return View(blogvalue);  
+
+        }
+
+    }
+}
