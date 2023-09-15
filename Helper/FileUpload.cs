1a2be41cbcaf38d3412bc5be916bf0db02ea1fff
sinan <sinancivak00@gmail.com>
2023-09-15 05:12:49 +0300

    Yardımcı katman oluşturuldu içeirisnden upload işlemi çekildi

---
+++ Helper/FileUpload.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class FileUpload
+    {
+        public static string UploadFile(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return "/images/" + fileName;
+            }
+            return null;
+        }
+
+    }
+}
