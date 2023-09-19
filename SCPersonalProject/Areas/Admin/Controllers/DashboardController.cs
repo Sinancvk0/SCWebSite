a0b60a0c03c63f2d4c5c529f250a0a1f11c06ef8
sinan <sinancivak00@gmail.com>
2023-09-19 08:03:28 +0300

    Loglama

--- SCPersonalProject/Areas/Admin/Controllers/DashboardController.cs
+++ SCPersonalProject/Areas/Admin/Controllers/DashboardController.cs
@@ -1,17 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using SC.DataLayer;
+using SC.Models;
 
 namespace SCPersonalProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DashboardController : Controller
     {
-      
 
+        private readonly AppDbContext _db;
+
+        public DashboardController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-           
-            return View();
+            var ipAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            var now = DateTime.Now;
+            var dateTime = DateTime.Now;
+
+            var visitorCount = new VisitorCount
+            {
+                DataDate = dateTime,
+                IpAdress = ipAddress
+
+            };
+            _db.VisitorCounts.Add(visitorCount);
+            _db.SaveChanges();
+
+            var count = _db.VisitorCounts.Where(x => x.DataDate == dateTime).Count();
+            var visitorCounts = _db.VisitorCounts.ToList();
+
+            ViewData["VisitorCounts"] = visitorCounts;
+
+            return View(count);
+
+
+
+            return View(count);
         }
+
+        private string GetIPAddress(string defaultValue = "UNKNOWN")
+        {
+            var ipAddress = HttpContext.Connection.RemoteIpAddress?.ToString() ?? defaultValue;
+            return ipAddress;
+        }
+
+
     }
 }
--- SCPersonalProject/Controllers/NewsApiController.cs
+++ SCPersonalProject/Controllers/NewsApiController.cs
@@ -9,7 +9,7 @@ namespace SCPersonalProject.Controllers
     [AllowAnonymous]
     public class NewsApiController : Controller
     {
-        private readonly string _apiKey = "8f6a5cbce9c24e7b991efbb5c20285db"; 
+        private readonly string _apiKey = "8f6a5cbce9c24e7b991efbb5c20285db";
         private readonly NewsApiClient _newsApiClient;
 
         public NewsApiController(NewsApiClient newsApiClient)
@@ -17,25 +17,38 @@ namespace SCPersonalProject.Controllers
             _newsApiClient = newsApiClient;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1, int pageSize = 100)
         {
+            // Calculate the offset based on the page and page size
+            int offset = (page - 1) * pageSize;
+
             var articlesResponse = _newsApiClient.GetEverything(new EverythingRequest
             {
-                Q = "software",
+                Q = "technology",
                 SortBy = SortBys.Popularity,
-                Language = Languages.TR
+                Language = Languages.TR,
+                Page = 1,           // Always fetch the first page of results
+                PageSize = pageSize // Set the number of articles per page
             });
 
             if (articlesResponse.Status == Statuses.Ok)
             {
-                var articles = articlesResponse.Articles;
-                return View(articles); 
+                var allArticles = articlesResponse.Articles;
+                var articles = allArticles.Skip(offset).Take(pageSize).ToList();
+
+                ViewBag.Page = page;
+                ViewBag.PageSize = pageSize;
+                ViewBag.TotalPages = (int)Math.Ceiling((double)allArticles.Count / pageSize);
+
+                return View(articles);
             }
             else
             {
-                return View("Error"); 
+                return View("Error");
             }
         }
+
+
     }
 }
 
